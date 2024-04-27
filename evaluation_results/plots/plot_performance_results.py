8d1345dff0bba90b96f44b67de799364fcf7abcd
Simon Schwitanski <schwitanski@itc.rwth-aachen.de>
2024-04-27 20:17:34 +0200

    Move plotting script

--- evaluation_results/plots/plot_performance_results.py
+++ evaluation_results/plots/plot_performance_results.py
@@ -22,20 +22,20 @@ benchmark_task_selector = {
 }
 
 rmasanitizer_benchmark_paths = {
-    "PRK_stencil": "../evaluation_results/RMASanitizer/PRK_stencil/result/result_csv.dat",
-    "BT-RMA": "../evaluation_results/RMASanitizer/BT-RMA/result/result_csv.dat",
-    "lulesh": "../evaluation_results/RMASanitizer/lulesh/result/result_csv.dat",
-    "miniMD": "../evaluation_results/RMASanitizer/miniMD/result/result_csv.dat",
-    "PRK_stencil_shmem": "../evaluation_results/RMASanitizer/PRK_stencil_shmem/result/result_csv.dat",
-    "BT-SHMEM": "../evaluation_results/RMASanitizer/BT-SHMEM/result/result_csv.dat",
-    "CFD-Proxy": "../evaluation_results/RMASanitizer/CFD-Proxy/result/result_csv.dat",
+    "PRK_stencil": "../RMASanitizer/PRK_stencil/result/result_csv.dat",
+    "BT-RMA": "../RMASanitizer/BT-RMA/result/result_csv.dat",
+    "lulesh": "../RMASanitizer/lulesh/result/result_csv.dat",
+    "miniMD": "../RMASanitizer/miniMD/result/result_csv.dat",
+    "PRK_stencil_shmem": "../RMASanitizer/PRK_stencil_shmem/result/result_csv.dat",
+    "BT-SHMEM": "../RMASanitizer/BT-SHMEM/result/result_csv.dat",
+    "CFD-Proxy": "../RMASanitizer/CFD-Proxy/result/result_csv.dat",
 }
 
 mustrma_benchmark_paths = {
-    "PRK_stencil": "../evaluation_results/MUST-RMA/PRK_stencil/result/result_csv.dat",
-    "BT-RMA": "../evaluation_results/MUST-RMA/BT-RMA/result/result_csv.dat",
-    "lulesh": "../evaluation_results/MUST-RMA/lulesh/result/result_csv.dat",
-    "miniMD": "../evaluation_results/MUST-RMA/miniMD/result/result_csv.dat",
+    "PRK_stencil": "../MUST-RMA/PRK_stencil/result/result_csv.dat",
+    "BT-RMA": "../MUST-RMA/BT-RMA/result/result_csv.dat",
+    "lulesh": "../MUST-RMA/lulesh/result/result_csv.dat",
+    "miniMD": "../MUST-RMA/miniMD/result/result_csv.dat",
 }
 
 plt.rc('pdf', fonttype=42)
