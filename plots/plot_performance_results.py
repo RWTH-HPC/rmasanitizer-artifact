46b2a9088880283253ac85b486da1668a1d7652b
Simon Schwitanski <schwitanski@itc.rwth-aachen.de>
2024-04-27 12:11:01 +0200

    Update plotting script, add MUST-RMA plots

--- plots/plot_performance_results.py
+++ plots/plot_performance_results.py
@@ -1,8 +1,7 @@
-from datetime import datetime
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import numpy as np
+import matplotlib.ticker as mticker
 import matplotlib.patches as mpatches
 import matplotlib.lines as mlines
 
@@ -13,26 +12,30 @@ benchmark_name_map = {
     "miniMD": "miniMD -- weak (MPI RMA)\n{400 timesteps, LJ, 260000 atoms per proc}",
     "PRK_stencil_shmem": "PRK Stencil -- weak (SHMEM)\n{1000 iters, $48 \cdot 10^6$ elements per proc }",
     "BT-SHMEM": "NPB BT -- strong (SHMEM)\n{Class D (162 x 162 x 162)}",
-    "CFD-Proxy": "CFD-Proxy - strong (GASPI)\n{4000 iters, F6 airplane mesh}"
+    "CFD-Proxy": "CFD-Proxy - strong (GASPI)\n{1000 iters, F6 airplane mesh}"
 }
 
 benchmark_task_selector = {
-    # "PRK_stencil": [48,192,768],
     "BT-RMA": [49,100,225,361,729],
     "BT-SHMEM": [49,100,225,361,729],
     "lulesh": [64,125,216,343,729],
-    # "miniMD": [48,192,768],
-    #"BT-SHMEM": [49,225,361]
 }
 
-benchmark_paths = {
-    "PRK_stencil": "../evaluation_results/PRK_stencil/result/result_csv.dat",
-    "BT-RMA": "../evaluation_results/BT-RMA/result/result_csv.dat",
-    "lulesh": "../evaluation_results/lulesh/result/result_csv.dat",
-    "miniMD": "../evaluation_results/miniMD/result/result_csv.dat",
-    "PRK_stencil_shmem": "../evaluation_results/PRK_stencil_shmem/result/result_csv.dat",
-    "BT-SHMEM": "../evaluation_results/BT-SHMEM/result/result_csv.dat",
-    "CFD-Proxy": "../evaluation_results/CFD-Proxy/result/result_csv.dat",
+rmasanitizer_benchmark_paths = {
+    "PRK_stencil": "../evaluation_results/RMASanitizer/PRK_stencil/result/result_csv.dat",
+    "BT-RMA": "../evaluation_results/RMASanitizer/BT-RMA/result/result_csv.dat",
+    "lulesh": "../evaluation_results/RMASanitizer/lulesh/result/result_csv.dat",
+    "miniMD": "../evaluation_results/RMASanitizer/miniMD/result/result_csv.dat",
+    "PRK_stencil_shmem": "../evaluation_results/RMASanitizer/PRK_stencil_shmem/result/result_csv.dat",
+    "BT-SHMEM": "../evaluation_results/RMASanitizer/BT-SHMEM/result/result_csv.dat",
+    "CFD-Proxy": "../evaluation_results/RMASanitizer/CFD-Proxy/result/result_csv.dat",
+}
+
+mustrma_benchmark_paths = {
+    "PRK_stencil": "../evaluation_results/MUST-RMA/PRK_stencil/result/result_csv.dat",
+    "BT-RMA": "../evaluation_results/MUST-RMA/BT-RMA/result/result_csv.dat",
+    "lulesh": "../evaluation_results/MUST-RMA/lulesh/result/result_csv.dat",
+    "miniMD": "../evaluation_results/MUST-RMA/miniMD/result/result_csv.dat",
 }
 
 plt.rc('pdf', fonttype=42)
@@ -43,7 +46,7 @@ plt.rcParams.update({
 
 plt.rc('text.latex', preamble=r'\usepackage[italic]{mathastext}')
 
-def read_csv(benchmark_name, csv_file) -> None:
+def read_csv(benchmark_name, csv_file) -> pd.DataFrame:
     # Read csv
     df = pd.read_csv(csv_file, sep=";")
     # Drop columns which have no values
@@ -51,40 +54,31 @@ def read_csv(benchmark_name, csv_file) -> None:
     # Fill empty cells with empty string
     df = df.fillna('None')
 
-    print(df)
-
-    # Drop all columns except tasks, compile, measurement time_avg, time_std must_compile_opt
-    df = df[['tasks', 'compile', 'measurement',
+    # Drop all columns except tasks, measurement time_avg, time_std
+    df = df[['tasks', 'measurement',
              'time_avg', 'time_std']]
     df['benchmark'] = benchmark_name
 
     # Calculate tool slowdown
     target_column = "time_avg"
-    df["tool slowdown"] = [row[target_column]/df.loc[(df['compile'] == "base")
-                                                     & (df['measurement'] == "base")
+    df["tool slowdown"] = [row[target_column]/df.loc[(df['measurement'] == "base")
                                                      & (df['tasks'] == row['tasks'])][target_column].iloc[0] for index, row in df.iterrows()]
     
     df["time_std_rel"] = df["time_std"] / df["time_avg"]
     target_column = "time_std_rel"
-    df["tool slowdown std rel"] = [row[target_column] + df.loc[(df['compile'] == "base")
-                                                     & (df['measurement'] == "base")
+    df["tool slowdown std rel"] = [row[target_column] + df.loc[(df['measurement'] == "base")
                                                      & (df['tasks'] == row['tasks'])][target_column].iloc[0] for index, row in df.iterrows()]
     df["tool slowdown std"] = df["tool slowdown"] * df["tool slowdown std rel"]
 
     df_base = df[df['measurement'] == "base"].rename(columns={"time_avg": "base_avg", "time_std": "base_std", "time_std_rel": "base_std_rel"}).reset_index()
-    df_rmasanitize = df[df['measurement'] == "must"].rename(columns={"time_avg": "rmasan_avg", "time_std": "rmasan_std", "time_std_rel": "rmasan_std_rel"}).reset_index()
+    df_rmasanitize = df[df['measurement'] == "must"].rename(columns={"time_avg": "tool_avg", "time_std": "rmasan_std", "time_std_rel": "rmasan_std_rel"}).reset_index()
     df = pd.concat([df_rmasanitize, df_base["base_avg"], df_base["base_std"], df_base["base_std_rel"]], axis=1)
-    df = df[["benchmark", "tasks", "base_avg", "rmasan_avg", "tool slowdown", "base_std", "rmasan_std", "tool slowdown std", "base_std_rel",  "rmasan_std_rel", "tool slowdown std rel"]]
-    print(df)
+    df = df[["benchmark", "tasks", "base_avg", "tool_avg", "tool slowdown", "base_std", "rmasan_std", "tool slowdown std", "base_std_rel",  "rmasan_std_rel", "tool slowdown std rel"]]
 
     return df
 
 
 def create_plots(dfs) -> None:
-    palette = ["#f0f0f0"]
-
-    # sns.set_style("whitegrid", rc={
-    #     "grid.linestyle": "solid", "grid.color": "white"})
     sns.set_theme(rc={"lines.linewidth": 0.8})
     plt.rc('axes', axisbelow=True)
     fig, axs = plt.subplots(ncols=4, nrows=2, figsize=(12, 3), constrained_layout=True)
@@ -94,11 +88,11 @@ def create_plots(dfs) -> None:
 
         # Only take those rows with task numbers that we are interested in
         if benchmark in benchmark_task_selector:
-            df = df[df['tasks'].isin(benchmark_task_selector[benchmark])]
+            df = df[df['tasks'].isin(benchmark_task_selector[benchmark])].copy()
 
         df['tasks'] = df['tasks'].astype(str)
 
-        df.plot.bar(x='tasks', y='tool slowdown', color=palette, edgecolor="black", width=0.8, rot=0, ax=ax)
+        df.plot.bar(x='tasks', y='tool slowdown', color=["#dedede"], edgecolor="black", width=0.8, rot=0, ax=ax)
         ax2 = ax.twinx()
 
         ax.get_legend().remove()
@@ -115,21 +109,22 @@ def create_plots(dfs) -> None:
         ax.spines["right"].set_visible(True)
         ax.yaxis.set_label_position('right')
         ax.yaxis.set_ticks_position('right')
-        if benchmark != "CFD-Proxy":
+        if benchmark not in ["CFD-Proxy", "BT-RMA"]:
             ax.set_ylim(0, 15)
 
-        # Show values
-        # for i in ax.containers:
-        #      texts = ax.bar_label(i, fmt='%.2f', rotation=0, fontsize=7)
-        #      for text in texts:
-        #          text.set(y=2, zorder=1000)
-
-        # ax2.set_zorder(10)
         ax2.set_ylabel("Runtime [s]", fontsize=8)
-        df.plot(kind='line', x='tasks', y='rmasan_avg', color='blue', ax=ax2, style='.-')
-        df.plot(kind='line', x='tasks', y='base_avg', color='red', ax=ax2, style='.-')
-
         
+        if benchmark in mustrma_benchmark_paths:
+            mustrma_df = read_csv(benchmark, mustrma_benchmark_paths[benchmark])
+            # Only take those rows with task numbers that we are interested in
+            if benchmark in benchmark_task_selector:
+                mustrma_df = mustrma_df[mustrma_df['tasks'].isin(benchmark_task_selector[benchmark])]
+            mustrma_df['tasks'] = mustrma_df['tasks'].astype(str)
+            mustrma_df.plot(kind='line', x='tasks', y='tool_avg', color='#ffa500', ax=ax2, style='.-')
+
+        df.plot(kind='line', x='tasks', y='tool_avg', color='#57ab27', ax=ax2, style='.-')
+        df.plot(kind='line', x='tasks', y='base_avg', color='#00549f', ax=ax2, style='.-')
+    
         # ================================
         # Prevent marker cutoff
         # =================================
@@ -138,8 +133,8 @@ def create_plots(dfs) -> None:
         # transform pixel markersize to markersize based on the axis
         markersize_data = (ax2.transData.inverted().transform((1, 1)) - ax2.transData.inverted().transform((0, 0)))[1]*markersize
         # calculate ymargin at the bottom such that the marker are not cutoff
-        ymargin_bottom = min(0, df['rmasan_avg'].min() - markersize_data, df['base_avg'].min() - markersize_data)
-        ax2.set_ylim(bottom=0 + ymargin_bottom, top=df['rmasan_avg'].max()*1.1)
+        ymargin_bottom = min(0, df['tool_avg'].min() - markersize_data, df['base_avg'].min() - markersize_data)
+        ax2.set_ylim(bottom=0 + ymargin_bottom, top=ax2.get_ylim()[1]*1.05)
         ax2.get_legend().remove()
 
         # axis on the left side
@@ -168,7 +163,7 @@ def create_plots(dfs) -> None:
         ax.grid(False)
         ax2.grid(False)
         
-        #Set ticks and limits of the background axis to the respective values of axis ax2  
+        #Set ticks and limits of the background axis to the respective values of axis ax2
         axBG.set_ylim(ax2.get_ylim())   
         # only show ticks that are in the limits and are larger than 0
         axBG.set_yticks([i for i in ax2.get_yticks().tolist() if i >= 0 and i <= axBG.get_ylim()[1]])
@@ -196,7 +191,8 @@ def create_plots(dfs) -> None:
         # Plot text on the twin axis
         for bar in ax.containers[0]:
             height = bar.get_height()
-            ax_text.text(bar.get_x() + bar.get_width() / 2., height + 1, '%.2f' % height, ha='center', fontsize=7)
+            ypos = height + (0.9*(ax.get_ylim()[1]/15))
+            ax_text.text(bar.get_x() + bar.get_width() / 2., ypos, '%.2f' % height, ha='center', fontsize=7)
 
         # Set axis ax back on the right side
         ax.spines["right"].set_visible(True)
@@ -211,11 +207,12 @@ def create_plots(dfs) -> None:
     axs[1,3].axis('off')
 
     # legend
-    blue_line = mlines.Line2D([], [], color='red', marker='.', markersize=8, label="Runtime without tool (baseline)")
-    red_line = mlines.Line2D([], [], color='blue', marker='.', markersize=8, label="Runtime with RMASanitizer")
-    gray_patch = mpatches.Patch(color='#f0f0f0', label='Tool Slowdown')
-    legend = fig.legend(handles=[blue_line, red_line, gray_patch], title="Legend", loc=(0.79,0.15), fontsize=9)
-    legend.get_title().set_fontsize(11)
+    base_line = mlines.Line2D([], [], color='#00549f', marker='.', markersize=8, label="Runtime without tool (baseline)")
+    mustrma_line = mlines.Line2D([], [], color='#ffa500', marker='.', markersize=8, label="Runtime with MUST-RMA")
+    rmasan_line = mlines.Line2D([], [], color='#57ab27', marker='.', markersize=8, label="Runtime with RMASanitizer")
+    gray_patch = mpatches.Patch(facecolor='#dedede', edgecolor='black', label='Tool Slowdown with RMASanitizer')
+    legend = fig.legend(handles=[base_line, mustrma_line, rmasan_line, gray_patch], title="Legend", loc=(0.79,0.15), fontsize=8)
+    legend.get_title().set_fontsize(10)
 
     plt.yticks(fontname="Helvetica")
 
@@ -224,6 +221,6 @@ def create_plots(dfs) -> None:
 
 if __name__ == "__main__":
     dfs = []
-    for (benchmark, path) in benchmark_paths.items():
+    for (benchmark, path) in rmasanitizer_benchmark_paths.items():
         dfs.append(read_csv(benchmark, path))
     create_plots(dfs)
