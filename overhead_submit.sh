301d45f683b69b077340747649da0c8539dfd7e6
Simon Schwitanski <schwitanski@itc.rwth-aachen.de>
2024-06-15 15:17:53 +0000

    Add MUST-RMA execution, update perf benchmark scripts

--- overhead_submit.sh
+++ overhead_submit.sh
@@ -1,15 +1,15 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 set -e
 
-benchmarks_mustrma=( PRK_stencil  BT-RMA  lulesh  miniMD )
-benchmarks_rmasanitizer=( PRK_stencil BT-RMA lulesh miniMD PRK_stencil_shmem BT-SHMEM CFD-Proxy )
+benchmarks_mustrma=(PRK_stencil  BT-RMA  lulesh  miniMD)
+benchmarks_rmasanitizer=(PRK_stencil BT-RMA lulesh miniMD PRK_stencil_shmem BT-SHMEM CFD-Proxy)
 for benchmark in ${benchmarks_mustrma[*]}
 do
-    jube run overhead_evaluation/jube/${benchmark}/${benchmark}.xml -o MUST-RMA/${benchmark} --tag M ignorelist pnmpi memusage rebuild_source must-rma
+    jube run overhead_evaluation/jube/benchmarks/${benchmark}/${benchmark}.xml -o perf-eval-result/MUST-RMA/${benchmark} --tag S ignorelist memusage rebuild_source must-rma chameleon
 done
 
 for benchmark in ${benchmarks_rmasanitizer[*]}
 do
-    jube run overhead_evaluation/jube/${benchmark}/${benchmark}.xml -o RMASanitizer/${benchmark} --tag M ignorelist pnmpi memusage rebuild_source tsan-opt
+    jube run overhead_evaluation/jube/benchmarks/${benchmark}/${benchmark}.xml -o perf-eval-result/RMASanitizer/${benchmark} --tag S ignorelist memusage rebuild_source tsan-opt chameleon
 done
