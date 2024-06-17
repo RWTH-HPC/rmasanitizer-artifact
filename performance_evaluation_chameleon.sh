2f5e0eed48c72d90659dabda9d7800dd3562ef90
Simon Schwitanski <schwitanski@itc.rwth-aachen.de>
2024-06-17 10:49:37 +0000

    Move Dockerfile, update input sizes

--- performance_evaluation_chameleon.sh
+++ performance_evaluation_chameleon.sh
@@ -2,11 +2,11 @@
 set -e
 
 echo "Building Docker image. This may take some time..."
-docker build -f chameleon/Dockerfile -t rmasan .
+docker build -f overhead_evaluation/chameleon/Dockerfile -t rmasan .
 echo "Build completed."
 
-# echo "Start container and run experiments..."
-# docker run -u $UID:$UID --rm --shm-size=128g --ulimit memlock=32939628:32939628 -v $PWD:/2024-icpp-rmasanitizer -it rmasan /bin/bash -c "cd /2024-icpp-rmasanitizer && ./overhead_submit.sh"
+echo "Start container and run experiments..."
+docker run -u $UID:$UID --rm --shm-size=128g --ulimit memlock=32939628:32939628 -v $PWD:/2024-icpp-rmasanitizer -it rmasan /bin/bash -c "cd /2024-icpp-rmasanitizer && ./overhead_submit.sh"
 
 echo "Gather results and plot them..."
 docker run -u $UID:$UID --rm --shm-size=128g --ulimit memlock=32939628:32939628 -v $PWD:/2024-icpp-rmasanitizer -it rmasan /bin/bash -c "cd /2024-icpp-rmasanitizer && ./overhead_results.sh"
